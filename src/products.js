0430a8cf07764844819bb7809905ca95b448ad70
Mark <datawithmark@gmail.com>
2024-03-20 11:50:18 +0530

    more products are added

--- src/products.js
+++ src/products.js
@@ -28,5 +28,48 @@ export const PRODUCTS = [
     productName: "Bajaj GX-1 Mixer Grinder",
     price: 2087,
     productImage: "https://m.media-amazon.com/images/I/51NNA+k9S0L._SL1500_.jpg"
-  }
+  },
+  {
+    id: 6,
+    productName: "Cookwell Bullet Mixer Grinder 600",
+    price: 2477,
+    productImage: "https://m.media-amazon.com/images/I/81yobRRV8nL._SL1500_.jpg"
+  }, 
+  {
+    id: 7,
+    productName: "Pigeon Healthifry Digital Air Fryer",
+    price: 3299,
+    productImage: "https://m.media-amazon.com/images/I/71l+9JWHRbL._SL1500_.jpg"
+  },
+  {
+    id: 8,
+    productName: "AGARO Ace Wet&Dry Vacuum Cleaner",
+    price: 6299,
+    productImage: "https://m.media-amazon.com/images/I/61B8-nfHqlL._SL1500_.jpg"
+  },
+  {
+    id: 9,
+    productName: "iSiLER Air Fryer Electric Oven",
+    price: 8299,
+    productImage: "https://m.media-amazon.com/images/I/81BWFsUxR5L._SL1500_.jpg"
+  },
+  {
+    id: 10,
+    productName: "Havells Electric Kettle Aqua Plus",
+    price: 1509,
+    productImage: "https://m.media-amazon.com/images/I/51EGS1jWNeS._SL1180_.jpg"
+  },
+  {
+    id: 11,
+    productName: "Voltas 1.4 Ton 3 Star Inverter Split AC",
+    price: 30990,
+    productImage: "https://m.media-amazon.com/images/I/51wIJED3K-L._SL1500_.jpg"
+  },
+  {
+    id: 12,
+    productName: "Eureka Forbes Quick Clean DX Vacuum Cleaner",
+    price: 3699,
+    productImage: "https://m.media-amazon.com/images/I/71JS5wMHfnL._SL1500_.jpg"
+  },
+  
 ]
